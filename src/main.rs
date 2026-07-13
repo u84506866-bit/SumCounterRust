6b809386fb6a161a475fee9ba1aedc34f5796203
U84506866-bit <u84506866@gmail.com>
2026-07-13 22:41:03 +0330

    final version with inputs

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,19 @@
+use std::io;
+
 fn main() {
-    let int1: i32 = 12;
-    let int2: i32 = 24;
+    let mut int1 = String::new();
+    let mut int2 = String::new();
+
+    io::stdin()
+        .read_line(&mut int1)
+        .expect("Failed to read line");
+
+    io::stdin()
+        .read_line(&mut int2)
+        .expect("Failed to read line");
+
+    let int1: i32 = int1.trim().parse().expect("Please Enter a Number!");
+    let int2: i32 = int2.trim().parse().expect("Please Enter a Number!");
     let int3 = sum(int1, int2);
     println!("{int3}");
 }
